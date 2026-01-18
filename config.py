f8fb5fff6b654bf86321938a5372e9405576f6cc
AZ TECH <168137282+Aztech-1729@users.noreply.github.com>
2026-01-18 06:13:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ Fill these values before running.
 """
 
 # Telegram Bot API token (from @BotFather)
-BOT_TOKEN = "8410490754:AAEcCDoGmEcsVqFYV6Dgizt0LU6UyLtRe3g"
+BOT_TOKEN = "8407662292:AAFWtt2AFyLu1NK25n5SN_AdaTujhVmH8-w"
 
 # Telethon API credentials (from https://my.telegram.org)
 API_ID = 33428535
 API_HASH = "c0dbd6f2553e9ed7ab51db2c6cd3360e"
 
 # Admin Telegram user IDs allowed to use this bot
-ADMIN_IDS = [7982816215,6670166083]
+ADMIN_IDS = [6670166083]
 
 # Telethon session name (used for a single shared user session)
 SESSION_NAME = "forward_user"
@@ -19,3 +19,4 @@ SESSION_NAME = "forward_user"
 # MongoDB connection
 MONGO_URI = "mongodb+srv://aztech:ayazahmed1122@cluster0.mhuaw3q.mongodb.net/forwardbot_db?retryWrites=true&w=majority"
 MONGO_DB_NAME = "forwardbot_db"
+
